4b46e5b573d88182039b256e8c0d1ff8948505b4
abdul-korim-web <moneyschorsing@gmail.com>
2026-08-26 15:29:30 +0600

    finance update api added

--- src/modules/finance/finance.route.ts
+++ src/modules/finance/finance.route.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from "express";
-import { createTransaction, deleteTransaction, getUserTransactions } from "./finance.controller.js";
+import { createTransaction, deleteTransaction, getUserTransactions, updateTransaction } from "./finance.controller.js";
 
 const router = Router();
 // POST API Endpoint
 router.post("/transactions", createTransaction);
 router.get("/transactions/:userId", getUserTransactions);
 router.delete("/transactions/:transactionId", deleteTransaction);
+router.patch("/transactions/:transactionId", updateTransaction); // PATCH route
 
 export default router;

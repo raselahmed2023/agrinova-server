1c283865f2553103f3fac120c13c64a9a895501d
Rasel Ahmed <raselahmediu22@gmail.com>
2026-09-03 08:48:41 +0600

    feat: complete need investment project submission

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,11 @@
 import { Router } from "express";
-
 import { FarmRoutes } from "../app/modules/farm/farm.route";
 import { WeatherRoutes } from "../app/modules/weather/weather.route";
-
 import aiRouter from "../modules/ai/ai.route.js";
-
 import { ProductRoutes } from "../modules/product/product.route";
-
 import financeRouter from "../modules/finance/finance.route.js";
-
 import { PurchaseRequestRoutes } from "../modules/purchase-request/purchaseRequest.route";
+import investmentRouter from "../modules/investment/investment.route.js";
 
 const router = Router();
 
@@ -50,6 +46,10 @@ const moduleRoutes = [
   {
     path: "/finance",
     route: financeRouter,
+  },
+  {
+    path: "/investments",
+    route: investmentRouter,
   },
 ];
 
